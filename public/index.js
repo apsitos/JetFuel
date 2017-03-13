334193a5e2edac22f1f6ce2733913ea60d847e96
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-03-13 15:40:59 -0600

    Folder name appends to page

--- public/index.js
+++ public/index.js
@@ -2,14 +2,14 @@ const folders = $('.folder-name');
 
 $('.create-folder').on('click', () => {
   let folderName = folders.val();
-  addFolder();
+  addFolder(folderName);
 })
 
-function addFolder() {
-  alert('Folder Append')
-  // $(.'folder-list').append(
-  //   `<li>
-  //     <h2>${folderName}</h2>
-  //   </li>`
-  // );
+function addFolder(folderName) {
+  alert('Folder Append');
+  $('.folder-list').append(
+    `<li>
+      <h2>${folderName}</h2>
+    </li>`
+  );
 }
