2229e2a6b614b58af6640a28d38fc30b90d06a72
Mike Ziccardi <mikeziccardi@gmail.com>
2017-03-17 08:25:59 -0600

    working on tests

--- tests/server-test.js
+++ tests/server-test.js
@@ -1,4 +1,4 @@
-const chai = require('chai');
+  const chai = require('chai');
 const expect = chai.expect;
 const app = require('../server.js')
 const chaiHttp = require('chai-http');
@@ -31,7 +31,7 @@ describe('Server', () => {
         if(err) {done(err)}
         expect(res).to.have.status(200)
         expect(res).to.be.json
-        expect(res.body).to.be.a('array')
+        expect(res.body).to.be.a('object')
         done()
       })
     })
@@ -45,7 +45,7 @@ describe('Server', () => {
         if(err){done(err);}
         expect(res).to.have.status(200)
         expect(res).to.be.json
-        expect(res.body).to.be.a('array')
+        expect(res.body).to.be.a('object')
         expect(res.body.length).to.equal()
         done()
       })
